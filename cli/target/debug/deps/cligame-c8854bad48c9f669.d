fdb8f778405fba13617df5a1bb1c34645b8b0a20
Matthew Franchini <matchewf11@gmail.com>
2025-11-11 22:48:51 -0800

    init commit

---
+++ cli/target/debug/deps/cligame-c8854bad48c9f669.d
@@ -0,0 +1,5 @@
+/home/matchew/Coding/cligame/cli/target/debug/deps/cligame-c8854bad48c9f669.d: src/main.rs
+
+/home/matchew/Coding/cligame/cli/target/debug/deps/libcligame-c8854bad48c9f669.rmeta: src/main.rs
+
+src/main.rs:
