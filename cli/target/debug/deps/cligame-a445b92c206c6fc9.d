f110ec0db4aad55acd0f494f25837a68448a754d
Matthew Franchini <matchewf11@gmail.com>
2025-11-11 22:50:35 -0800

    fixed up repo

--- cli/target/debug/deps/cligame-a445b92c206c6fc9.d
+++
@@ -1,9 +0,0 @@
-/home/matchew/Coding/cligame/cli/target/debug/deps/cligame-a445b92c206c6fc9.d: src/main.rs Cargo.toml
-
-/home/matchew/Coding/cligame/cli/target/debug/deps/libcligame-a445b92c206c6fc9.rmeta: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
--- cli/target/debug/deps/cligame-f3be46231927f32c.d
+++
@@ -1,5 +0,0 @@
-/home/matchew/Coding/cligame/cli/target/debug/deps/cligame-f3be46231927f32c.d: src/main.rs
-
-/home/matchew/Coding/cligame/cli/target/debug/deps/libcligame-f3be46231927f32c.rmeta: src/main.rs
-
-src/main.rs:
